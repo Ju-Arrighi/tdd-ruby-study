c2139b626fe8bb245f57edc989173794f07b1b10
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-02-09 16:03:12 +0100

    comparasion matchers

--- exemplo_rspec_tdd/spec/matchers/comparacao_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/comparacao_spec.rb
@@ -1,28 +1,37 @@
-describe 'comparacao' do
-  context '#testa objeto' do
-    it 'equal' do
-      x = "ruby"
-      y = "ruby"
-      expect(x).not_to equal(y)
-    end
-
-    it 'be' do
-      x = "ruby"
-      y = "ruby"
-      expect(x).not_to be(y)
-    end
+describe 'comparação' do
+  it '<' do
+    expect(5).to be < 10
   end
-  context '#testa valor' do
-    it 'eql' do
-      x = "ruby"
-      y = "ruby"
-      expect(x).to eql(y)
-    end
 
-    it 'eq' do
-      x = "ruby"
-      y = "ruby"
-      expect(x).to eq(y)
-    end
+  it '>' do
+    expect(5).to be > 1
+  end
+
+  it '<=' do
+    expect(5).to be <= 10
+    expect(5).to be <= 5
+  end
+
+  it '>=' do
+    expect(10).to be >= 10
+    expect(10).to be >= 5
+  end
+
+  it 'be_between' do
+    expect(10).to be_between(5,20)
+    expect(10).to be_between(10,20).inclusive
+    expect(10).to be_between(9,20).exclusive
+  end
+
+  it 'regex' do
+    expect('fulano@gmail.com').to match('..@..')
+  end
+
+  it 'start_with' do
+    expect('fulano@gmail.com').to start_with('fulano')
+  end
+
+  it 'end_with' do
+    expect('fulano@gmail.com').to end_with('com')
   end
 end
---
+++ exemplo_rspec_tdd/spec/matchers/igualdade_spec.rb
@@ -0,0 +1,28 @@
+describe 'igualdade' do
+  context '#testa objeto' do
+    it 'equal' do
+      x = "ruby"
+      y = "ruby"
+      expect(x).not_to equal(y)
+    end
+
+    it 'be' do
+      x = "ruby"
+      y = "ruby"
+      expect(x).not_to be(y)
+    end
+  end
+  context '#testa valor' do
+    it 'eql' do
+      x = "ruby"
+      y = "ruby"
+      expect(x).to eql(y)
+    end
+
+    it 'eq' do
+      x = "ruby"
+      y = "ruby"
+      expect(x).to eq(y)
+    end
+  end
+end
