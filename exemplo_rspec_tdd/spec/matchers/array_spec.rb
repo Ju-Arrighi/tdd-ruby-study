6df46dffc943f48a29790d6a0765e658a552c640
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-02-09 23:53:26 +0100

    matcher for arrays

---
+++ exemplo_rspec_tdd/spec/matchers/array_spec.rb
@@ -0,0 +1,11 @@
+describe [1, 2, 3], 'Array' do
+  it 'include' do
+    expect(subject).to include(1)
+  end
+  it 'contain_exactly' do
+    expect(subject).to contain_exactly(2, 1, 3)
+  end
+  it 'match_array' do
+    expect(subject).to match_array([1, 2, 3])
+  end
+end
