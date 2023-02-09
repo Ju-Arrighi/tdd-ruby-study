c9e956b46c7d1b7da278134c9105d9eae527a438
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-02-09 21:48:49 +0100

    matcher raise error

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,6 +1,17 @@
 require 'calculator'
-describe Calculator, "Ex of calcultor" do
+
+describe Calculator, 'Ex of calcultor' do
   subject(:calc) { Calculator.new } # { described_class.new() }
+
+  context '#div' do
+    it 'div by 0'do
+      expect { subject.div(3, 0) }.to raise_exception
+      expect { subject.div(3, 0) }.to raise_error(ZeroDivisionError)
+      expect { subject.div(3, 0) }.to raise_error('divided by 0')
+      expect { subject.div(3, 0) }.to raise_error(/divided/)
+    end
+  end
+
   context '#sum' do
     it 'with positive numbers' do
       result = calc.sum(2,7)
