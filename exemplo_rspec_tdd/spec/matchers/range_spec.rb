009f110609c6f1751556fb569c06c2f4d6373547
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-02-10 12:52:50 +0100

    matcher composition

--- exemplo_rspec_tdd/spec/matchers/range_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/range_spec.rb
@@ -3,6 +3,5 @@ describe (1..5), 'range' do
     # expect(subject).to cover(2,5) REESCRITO
     expect(subject).not_to cover(0,6)
   end
-
   it { is_expected.to cover(2,5) }  # One-liner Syntex
 end
