bf65cc3dbde92a2a2eeae9fb1dca03a1b746fe92
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-02-10 12:33:58 +0100

    One-liner Syntex

--- exemplo_rspec_tdd/spec/matchers/range_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/range_spec.rb
@@ -1,6 +1,8 @@
 describe (1..5), 'range' do
   it 'cover' do
-    expect(subject).to cover(2,5)
+    # expect(subject).to cover(2,5) REESCRITO
     expect(subject).not_to cover(0,6)
   end
+
+  it { is_expected.to cover(2,5) }  # One-liner Syntex
 end
